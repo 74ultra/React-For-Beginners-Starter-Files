bab0240b92560c368b2fb9d01a48ed3c46424f50
74ultra <r.barnhill@protonmail.com>
2019-08-16 22:34:07 -0700

    God fucking dammit

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+const Header = ({tagline}) => {
+    return (
+        <header className="top">
+            <h1>
+                Catch
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                Day
+            </h1>
+            <h3 className="tagline">
+                <span>{tagline}</span>
+            </h3>
+        </header>
+    )
+}
+
+
+
+
+/*
 class Header extends React.Component {
     render(){
+        
         return(
         <header className="top">
             <h1>
@@ -13,11 +36,13 @@ class Header extends React.Component {
                 Day
             </h1>
             <h3 className="tagline">
-                <span>Fresh Daily</span>
+                <span>{this.props.tagline}</span>
             </h3>
         </header>
         )
     }
 }
+
+*/
 
 export default Header;
